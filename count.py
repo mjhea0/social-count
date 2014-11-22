a6477b3961bbd77a595a77b7ac76ac09670d183b
Michael Herman <hermanmu@gmail.com>
2014-11-21 21:54:01 -0700

    initial

--- count.py
+++ count.py
@@ -8,27 +8,33 @@ LINKEDIN = "https://www.linkedin.com/countserv/count/share?url={0}&format=json"
 
 def get_twitter(url):
     request = urllib2.urlopen(TWITTER.format(url))
-    jsonLoad = json.loads(request.read())
+    results = json_decode(request)
     print "\nTwitter:"
-    print "Tweets:", jsonLoad['count']
+    print "Tweets:", results['count']
 
 
 def get_facebook(url):
     request = urllib2.urlopen(FACEBOOK.format(url))
-    jsonLoad = json.loads(request.read())
+    results = json_decode(request)
     print "\nFacebook:"
-    print "Shares:", jsonLoad[0]['share_count']
-    print "Likes:", jsonLoad[0]['like_count']
-    print "Comments:", jsonLoad[0]['comment_count']
-    print "Clicks:", jsonLoad[0]['click_count']
-    print "TOTAL:", jsonLoad[0]['total_count']
+    print "Shares:", results[0]['share_count']
+    print "Likes:", results[0]['like_count']
+    print "Comments:", results[0]['comment_count']
+    print "Clicks:", results[0]['click_count']
+    print "TOTAL:", results[0]['total_count']
 
 
 def get_linkedin(url):
     request = urllib2.urlopen(LINKEDIN.format(url))
-    jsonLoad = json.loads(request.read())
+    results = json_decode(request)
     print "\nLinkedIn:"
-    print "Shares:", jsonLoad['count']
+    print "Shares:", results['count']
+
+
+# helper
+
+def json_decode(request):
+    return json.loads(request.read())
 
 
 if __name__ == '__main__':
